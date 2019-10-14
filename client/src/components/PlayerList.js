9350ab1b6b45e78dc4e6bc292fcb98393dadd1ca
Kurt Van Etten <kurt@vanetten.us>
2019-10-13 20:47:07 -0400

    Add paging buttons to PlayerList component

--- client/src/components/PlayerList.js
+++ client/src/components/PlayerList.js
@@ -5,6 +5,30 @@ import Player from './Player';
 
 
 
+const ButtonWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-around;
+	width: 300px;
+	padding-top: 20px;
+
+	button {
+		margin: 0px 20px;
+		height: 30px;
+		width: 60px;
+		border-radius: 5px;
+		border: none;
+		font-size: 1.4rem;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+
+`;
+
+
 const ListWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
@@ -16,12 +40,30 @@ const ListWrapper = styled.div`
 `;
 
 
+
+
 const PlayerList = (props) => {
 
+
+	const goBack = () => {
+		console.log('go back 1 page');
+	};
+
+	const goForward = () => {
+		console.log('go forward 1 page');
+	}
+
+
 	return (
-		<ListWrapper>
-			{props.players.map(player => <Player key={player.id} player={player} /> )}
-		</ListWrapper>
+		<>
+			<ButtonWrapper>
+				<button onClick={goBack} className='back'>Back</button>
+				<button onClick={goForward} className='next'>Next</button>
+			</ButtonWrapper>
+			<ListWrapper>
+				{props.players.map(player => <Player key={player.id} player={player} /> )}
+			</ListWrapper>
+		</>
 	);
 };
 
